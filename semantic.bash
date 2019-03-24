d19547e3b8ce75980e618253669ae47c7dfba12f
ApolloLiZhaoyu <h084117lzy@126.com>
2019-03-24 18:02:36 +0800

    fix bash

--- semantic.bash
+++ semantic.bash
@@ -1,5 +1,5 @@
 set -e
 cd "$(dirname "$0")"
-export Mxstar="java -classpath ./lib/antlr-runtime-4.7.2.jar:./bin src.Main"
+export Mxstar="java -classpath ./lib/antlr-runtime-4.7.2.jar:./bin Main"
 cat > program.cpp   # save everything in stdin to astProgram.txt
 $Mxstar
