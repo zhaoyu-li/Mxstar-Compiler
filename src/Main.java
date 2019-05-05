fab73e3f8dddfeeb080b0b3a12104394d70a344c
ApolloLiZhaoyu <h084117lzy@126.com>
2019-05-06 01:34:03 +0800

    fix ChordalGraph

--- src/Main.java
+++ src/Main.java
@@ -62,9 +62,9 @@ public class Main {
         NASMTransformer nasmTransformer = new NASMTransformer();
         nasmTransformer.visit(irProgram);
 
-        IRPrinter irPrinter = new IRPrinter();
-        irPrinter.visit(irProgram);
-        irPrinter.print();
+//        IRPrinter irPrinter = new IRPrinter();
+//        irPrinter.visit(irProgram);
+//        irPrinter.print();
 
 //        SimpleAllocator simpleAllocator = new SimpleAllocator(irProgram);
 //        simpleAllocator.allocateRegisters();
